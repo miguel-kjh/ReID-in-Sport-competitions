1c5910919bcd34d145944a4abc241a928c4590c8
miguel-kjh <miguelangel.kjh@gmail.com>
2021-01-16 17:05:23 +0000

    restructured data in a new file

--- data/move_files.py
+++ data/move_files.py
@@ -13,7 +13,6 @@ def main():
     for index in range(1,822):
         for dirpath, dirnames, filenames in os.walk(os.path.join(folder, str(index))):
             for filename in filenames:
-                print()
                 if _isImage(filename):
                     match = re.search(r'\d+_(\w+)_f', filename)
                     name = match.group(1)
